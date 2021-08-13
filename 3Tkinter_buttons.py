b3e0cfe3c380b2bb3fb454349db7b25747f73f11
TANNU KUMARI <tannuchoudhary10@gmail.com>
2021-08-13 16:44:08 +0530

    Add comment about place() function

--- 3Tkinter_buttons.py
+++ 3Tkinter_buttons.py
@@ -11,6 +11,7 @@ btn = Button(window, bd=20, bg="Red", fg="white", text="Click Me",
              padx=50,  pady=80)
 
 # we can use place instead of pack
+# with place, we can pass the parameters in the form of coedinates where we can define where we wan to place the button
 btn.place(x=50, y=50)
 
 window.mainloop()
